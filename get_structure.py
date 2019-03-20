183bab4d1a77dbe51ea706b5d377b146c74a1a92
patrickbryant1 <pbryant@ae18a>
2019-03-20 15:13:58 +0100

    updated to get from user defined website

--- get_structure.py
+++ get_structure.py
@@ -4,7 +4,6 @@
 
 import argparse
 import sys
-import pandas as pd
 import subprocess
 import pdb
 
@@ -13,15 +12,34 @@ parser = argparse.ArgumentParser(description = '''A program that downloads pdb s
  
 parser.add_argument('file_path', nargs=1, type= str,
                   default=sys.stdin, help = 'path to pdb ids file. Structure: #uid	pdb_id.')
+parser.add_argument('address', nargs=1, type= str,
+                  default=sys.stdin, help = 'Web adress to download from.')
 
+
+#Read tsv file 
+def read_groups(file_path):
+	'''Read tsv file into list
+	'''
+
+	uids = [] #Store uids
+	pdb_ids = [] #Store pdb_ids
+
+	with open(file_path) as file:
+		for line in file:
+			line = line.rstrip() #remove \n
+			line = line.split('\t')
+			uids.append(line[0])
+			pdb_ids.append(line[1])
+
+	return(uids, pdb_ids)
+
+#####MAIN#####
 args = parser.parse_args()
 
 file_path = args.file_path[0]
+address = args.address[0]
 
-#Read tsv file as pandas dataframe
-df = pd.read_csv(file_path, sep='\t')#get_ids
-
-pdb_ids = df['pdb']
+(uids, pdb_ids) = read_groups(file_path)
 
 #wget http://www.rcsb.org/pdb/files/1ZMQ.pdb.gz
 database_path = 'http://www.rcsb.org/pdb/files/'
