b69ce3c389324daa6ae5850dc8a58b24dcf56420
patrickbryant1 <pbryant@ae18a>
2019-03-21 10:36:49 +0100

    updated to only download n random entries in each H-group

--- get_structure.py
+++ get_structure.py
@@ -6,15 +6,39 @@ import argparse
 import sys
 import subprocess
 import pdb
+import glob
+import shutil
+import random
+
 
 #Arguments for argparse module:
 parser = argparse.ArgumentParser(description = '''A program that downloads pdb structures based on pdb ids.''')
  
-parser.add_argument('file_path', nargs=1, type= str,
-                  default=sys.stdin, help = 'path to pdb ids file. Structure: #uid	pdb_id.')
+parser.add_argument('input_dir', nargs=1, type= str,
+                  default=sys.stdin, help = 'path to pdb ids file directory. Structure of files: #uid	pdb_id.')
 parser.add_argument('address', nargs=1, type= str,
                   default=sys.stdin, help = 'Web adress to download from.')
+parser.add_argument('output_dir', nargs=1, type= str,
+                  default=sys.stdin, help = 'output directory.')
+parser.add_argument('selected_files', nargs=1, type= str,
+                  default=sys.stdin, help = 'Newline separated file with file names for files with ids to get structures for.')
+parser.add_argument('n_entries', nargs=1, type= int,
+                  default=sys.stdin, help = 'Number of entreis to get for each H-group.')
 
+
+
+
+def read_selected(selected_files):
+	'''Read newline separated filenames into list
+	'''
+
+	selected = []
+	with open(selected_files) as file:
+		for line in file:
+			line = line.rstrip() #remove \n
+			selected.append(line)
+
+	return(selected)
 
 #Read tsv file 
 def read_groups(file_path):
@@ -33,16 +57,46 @@ def read_groups(file_path):
 
 	return(uids, pdb_ids)
 
+def get_structures(address, file_name, input_dir, output_dir, n_entries, downloaded_ids):
+	'''Download .pdb structure and group into directory
+	'''
+	file_path = input_dir + file_name
+	(uids, pdb_ids) = read_groups(file_path)
+	positions = random.sample(range(0, len(uids)), n_entries) #Get n_entries random positions to download files for these ids
+
+	dir_name = file_name.split('.txt')[0] #Get directory name (X.H group)
+	subprocess.call(['mkdir', dir_name])
+
+	for i in positions:
+		downloaded_ids.append(pdb_ids[i])
+		subprocess.call(["wget",address+pdb_ids[i]])
+
+	for file in glob.glob(output_dir + 'str*'):
+		shutil.move(file, output_dir+dir_name)
+
+	
+	return downloaded_ids
+
 #####MAIN#####
 args = parser.parse_args()
 
-file_path = args.file_path[0]
+input_dir = args.input_dir[0]
 address = args.address[0]
+output_dir = args.output_dir[0]
+selected_files = args.selected_files[0]
+n_entries = args.n_entries[0]
 
-(uids, pdb_ids) = read_groups(file_path)
+#Get selected file names:
+selected = read_selected(selected_files)
 
-#wget http://www.rcsb.org/pdb/files/1ZMQ.pdb.gz
-for i in pdb_ids:
-	subprocess.call(["wget",address+i])
+downloaded_ids = [] #Keep track of ids that have been downloaded
+for file_name in selected:
+	downloaded_ids = get_structures(address, file_name, input_dir, output_dir, n_entries, downloaded_ids)
+
+#Print downloaded ids
+print(downloaded_ids)
+for downloaded_id in downloaded_ids:
+	print(downloaded_id)
+
 
 
