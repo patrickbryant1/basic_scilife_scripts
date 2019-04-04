5500a42c5b4b541571e26bfa2b4f98d8a6b200c8
patrickbryant1 <patrick.bryant@live.com>
2019-04-04 16:12:18 +0200

    saves numpy matrix as float

---
+++ RNN/rnn_input.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+def read_tsv(tsv_file, threshold):
+	'''Read tsv file format containing: uid1 \t uid2 \t ML distance \t RMSD distance
+	'''
+
+	uids = [] #Store uids
+	rmsd_dists = [] #Store rmsd distances
+	
+	with open(tsv_file) as file:
+		for line in file:
+			line = line.rstrip() #Remove newlines
+			line = line.split("\t")
+			ML_dist = round(float(line[2]), 2)
+			if ML_dist <= threshold:
+				rmsd_dists.append(float(line[3]))
+				
+				uids.append(line[0]+'_'+line[1])				
+			else:
+				continue
+
+	
+	return(uids, rmsd_dists)
+
+
+def rmsd_hot(rmsd_dists):
+	'''Normalize rmsd distances and convert to one-hot encodings
+	'''
+
+	#convert to numpy array
+	rmsd_dists = np.array(rmsd_dists)
+	#Normalize due to max rmsd
+	rmsd_dists = rmsd_dists/max(rmsd_dists)
+	#Multiply with 100
+	rmsd_dists = rmsd_dists*100
+	#Round to 0 decimal pints
+	rmsd_dists = np.around(rmsd_dists, decimals = 0)
+	#Convert to ints
+	rmsd_dists = rmsd_dists.astype(int)
+	#Convert to one-hot encoding
+	rmsd_dists_hot = np.eye(101)[rmsd_dists]
+
+
+	return rmsd_dists_hot
---
+++ RNN/rnn_model.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import argparse
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+
+#import custom functions
+from rnn_input import read_tsv, rmsd_hot
+import pdb
+
+
+
+
+#Arguments for argparse module:
+parser = argparse.ArgumentParser(description = '''A Recurrent Neural Network for predicting
+								RMSD between structural alignments based on sequences from per-residue alignments,
+								secondary structure and surface accessibility.''')
+ 
+parser.add_argument('dist_file', nargs=1, type= str,
+                  default=sys.stdin, help = 'path to distance file. Format: uid1	uid2	MLdist	RMSD')
+
+parser.add_argument('one_hot_dir', nargs=1, type= str,
+                  default=sys.stdin, help = 'path to files with one-hot encodings of alignments, sendary structure and surface acc.')
+
+#Functions
+
+
+#MAIN
+args = parser.parse_args()
+dist_file = args.dist_file[0]
+one_hot_dir = args.one_hot_dir[0]
+#Read tsv
+(uids, rmsd_dists) = read_tsv(dist_file, 6)
+#Format rmsd_dists into one-hot encoding
+rmsd_dists_hot = rmsd_hot(rmsd_dists)
+
+
+#Get macthing alignments, sendary structure and surface acc
+for i in range(0,len(uids)):
+	file = glob.glob(one_hot_dir + '*/'+uids[i]+'.hot')
+
+	if file:
+		pdb.set_trace()
+
+	#raise IOerror if file is not found
+
+
+
+
+
+#When splitting data. Make plots to see rmsd distribution is obtained in train, valid and test
