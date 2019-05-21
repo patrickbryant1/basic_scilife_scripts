3d13905930e74dac5b0a20e5c27ad83455eb7c64
Patrick Bryant <pbryant@b-an01.hpc2n.umu.se>
2019-05-21 12:26:26 +0200

    switched to pexpect

--- CATH/hh_suite.py
+++ CATH/hh_suite.py
@@ -5,6 +5,7 @@
 import argparse
 import sys
 import os
+import pexpect
 import subprocess
 import pdb
 
@@ -36,7 +37,7 @@ def pdb_to_fasta(uid, outdir):
 		while i<len(sequence):
 			outfile.write(sequence[i:i+60]+'\n')
 			i+=60
-	pdb.set_trace()
+
 	return None
 
 def run_hhblits(uid, outdir, hhblits, uniprot):
@@ -46,7 +47,7 @@ def run_hhblits(uid, outdir, hhblits, uniprot):
 
 	inname = uid+'.fa'
 	outname = uid+'.hhm'
+	statement = hhblits +' -i ' +outdir+inname + ' -d ' + uniprot + ' -ohhm ' + outname
+	pexpect.run(statement)
 	pdb.set_trace()
-	subprocess.call([hhblits +' -i ' +inname + ' -d ' + uniprot + ' -ohhm ' + outname])
-
 	return None
