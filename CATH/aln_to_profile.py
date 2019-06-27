e809eeda1b9abc0911daaf90000f6f12fd7ad8f7
Patrick Bryant <pbryant@b-an01.hpc2n.umu.se>
2019-06-27 10:25:54 +0200

    scripts to obtain hmm of alignment and to parse hmm

---
+++ CATH/aln_to_profile.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import argparse
+import sys
+
+#Custom import
+from parse_hmm import read_hmm
---
+++ CATH/parse_hmm.py
@@ -0,0 +1,42 @@
+import sys
+import pdb
+
+def read_hmm(afile):
+
+    hmm_list = []
+    null_model = []
+    transition_freq = []
+    local_div = []
+    in_hmm = False
+
+    for line in afile:
+        line = line.strip()
+        if line.startswith('NULL'):
+            null_model = line.split()[1:]
+            
+
+        if line.startswith('HMM'):
+            in_hmm = True
+            continue
+
+        if in_hmm:
+            line_arr = line.split()
+            if len(line_arr) == 10: #Not amino acid frequencies --> transition freq
+                transition_freq.append(line_arr[0:7])
+		local_div.append(line_arr[7:])
+            
+	    if len(line_arr) == 23:
+	        aa = line_arr[0]
+                freq = line_arr[2:22]
+                hmm_list.append((aa, freq))
+            
+    return hmm_list, null_model, transition_freq, local_div
+
+
+if __name__ == "__main__":
+
+    afile = open(sys.argv[1], 'r')
+    hmm_list, null_model, transition_freq, local_div = read_hmm(afile) #The two first entries of the transition_freq and local_div will belong to the null model
+    afile.close()
+    pdb.set_trace()
+    #print hmm_list
