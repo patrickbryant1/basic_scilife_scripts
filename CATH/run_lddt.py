8ad5e2dd39e4ef2f3a44f0c90cb4b4e3743745ec
Patrick Bryant <pbryant@b-an01.hpc2n.umu.se>
2019-07-09 11:04:01 +0200

    run lddt

---
+++ CATH/run_lddt.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import argparse
+import sys
+import os
+import subprocess
+import glob
+import pdb
+
+#Arguments for argparse module:
+parser = argparse.ArgumentParser(description = '''A program that renumbers the entrties in pdb files
+						and then runs lddt on the pdb files.''')
+
+parser.add_argument('indir', nargs=1, type= str,
+default=sys.stdin, help = 'path to directory with pdb files.')
+
+
+def run_lddt(indir):
+	'''Renumber pdb files to start with atom 0, residue 1.
+	'''
+
+	pdb_files = glob.glob(indir +'*_aln.pdb')
+	
+	#Run LDDT
+	while pdb_files:
+		pdb_name1 = pdb_files[0].split('/')[1]
+		uid1 = pdb_name1.split('_')[0]
+		uid2 = pdb_name1.split('_')[2]
+		
+		for i in range(1, len(pdb_files)):
+			if uid1 in pdb_files[i] and uid2 in pdb_files[i]:
+				break
+		pdb_name2 = pdb_files[i].split('/')[1]
+		command = 'singularity run --app lDDT /home/p/pbryant/pfs/singularity/ost.img -c -x -t ' + pdb_name1 + ' '+pdb_name2
+		try:
+			out = subprocess.check_output(command, shell = True)#Save parsed pdb
+		except:
+			continue
+		#Write to file
+		write_lddt(indir, out, uid1, uid2)
+
+		#Pop used pdb files
+		pdb_files.pop(i)
+		pdb_files.pop(0)
+		
+
+	return None
+
+def write_lddt(indir, out, uid1, uid2):
+	'''Writes lddt output to file
+	'''
+	aln_files = glob.glob(indir +'.aln')
+	for name in aln_files:
+		if uid1 in name and uid2 in name:
+			break
+	#write to file
+	lddt_name = name.split('/')[-1].split('.')[0]+'.lddt'
+	with open(lddt_name, 'w') as file:
+		file.write(out)
+
+	return None
+
+
+	
+#####MAIN#####
+args = parser.parse_args()
+indir = args.indir[0]
+run_lddt(indir)
+
